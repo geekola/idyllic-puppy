6f79a79575bc1df8be914a23c7a4678432ff14ce
Christine Siebels <christine@mygeekola.com>
2024-12-30 15:44:30 -0700

    Update Logo.tsx

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -8,7 +8,7 @@ export function Logo({ className = "h-6 w-auto" }: LogoProps) {
   return (
     <div className="flex items-center space-x-2">
       <LayoutGrid className={`text-primary ${className}`} />
-     <img src="../images/logo-overlay.png" alt="Spatial Mods" className='h-10' /
+     <img src="../images/logo-overlay.png" alt="Spatial Mods" className='h-10' />
     </div>
   );
 }
